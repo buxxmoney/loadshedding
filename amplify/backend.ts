069ae41a0f76ff0078ab960ce91835a563651ea6
buxxmoney <87129838+buxxmoney@users.noreply.github.com>
2025-03-12 14:40:09 -0700

    sandbox working

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,12 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const { groups } = backend.auth.resources
+
+// https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_iam.IRole.html
+groups["ADMINS"].role
